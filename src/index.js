9849d83bb25589cab07a245f80fea24224160b06
Lubov Fedorova <luba.zaverugo@gmail.com>
2021-02-22 16:47:07 +0300

    impl

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    let num = Math.abs(n);
+    let str = num.toString();
+      let arr = str.split('');
+      let result = arr.reverse().join('');
+      return result;
 }
